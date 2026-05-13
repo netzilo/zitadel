9bb866255604662fd171346e8b524bf8ef447a3d
Egemen <egemen@netzilo.com>
2026-05-13 13:21:47 +0300

    Update protobuf

--- console/src/main.ts
+++ console/src/main.ts
@@ -1,10 +1,3 @@
-// Polyfill: newer protoc-gen-js generates readStringRequireUtf8() calls, but the
-// google-protobuf npm runtime only ships readString(). They are equivalent for our use.
-import * as jspb from 'google-protobuf';
-if (!(jspb.BinaryReader.prototype as any).readStringRequireUtf8) {
-  (jspb.BinaryReader.prototype as any).readStringRequireUtf8 = jspb.BinaryReader.prototype.readString;
-}
-
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
